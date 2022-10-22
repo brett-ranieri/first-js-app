e9dde7df252b30118b4365c956a23f675b38e8cc
Brett Ranieri <brett.ranieri@gmail.com>
2022-10-22 11:19:37 -0600

    add initial pokemon list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello World');
-
-let favoriteFood = 'Spaghetti and Meat Sauce';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', type: ['Grass', 'poison'], height: '.7 m'},
+    {name: 'Ivysaur', type: ['Grass', 'poison'], height: '1 m'},
+    {name: 'Venusaur', type: ['Grass', 'poison'], height: '2 m'},
+    {name: 'Charmander', type: 'Fire', height: '.6 m'},
+    {name: 'Charmeleon', type: 'Fire', height: '1.1 m'},
+    {name: 'Charizard', type: ['Fire', 'Flying'], height: '1.7 m'},
+    {name: 'Squirtle', type: 'Water', height: '.5 m'},
+    {name: 'Wartortle', type: 'Water', height: '1 m'},
+    {name: 'Blastoise', type: 'Water', height: '1.6 m'},
+];
