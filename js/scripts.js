5ddaed0b7b0c6de7b417faccfbe66a6dc1acd9cc
Brett Ranieri <brett.ranieri@gmail.com>
2022-10-27 17:08:10 -0600

    add for loop to display name, height of each pokemon
    also labels the largest Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 let pokemonList = [
-    {name: 'Bulbasaur', type: ['Grass', 'poison'], height: '.7 m'},
-    {name: 'Ivysaur', type: ['Grass', 'poison'], height: '1 m'},
-    {name: 'Venusaur', type: ['Grass', 'poison'], height: '2 m'},
-    {name: 'Charmander', type: 'Fire', height: '.6 m'},
-    {name: 'Charmeleon', type: 'Fire', height: '1.1 m'},
-    {name: 'Charizard', type: ['Fire', 'Flying'], height: '1.7 m'},
-    {name: 'Squirtle', type: 'Water', height: '.5 m'},
-    {name: 'Wartortle', type: 'Water', height: '1 m'},
-    {name: 'Blastoise', type: 'Water', height: '1.6 m'},
+    {name: 'Bulbasaur', type: ['Grass', 'poison'], height: .7},
+    {name: 'Ivysaur', type: ['Grass', 'poison'], height: 1},
+    {name: 'Venusaur', type: ['Grass', 'poison'], height: 2},
+    {name: 'Charmander', type: 'Fire', height: .6},
+    {name: 'Charmeleon', type: 'Fire', height: 1.1},
+    {name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7},
+    {name: 'Squirtle', type: 'Water', height: .5},
+    {name: 'Wartortle', type: 'Water', height: 1},
+    {name: 'Blastoise', type: 'Water', height: 1.6},
 ];
+
+
+let big = '- Wow, that\'s big!';
+for (let i=0; i < pokemonList.length; i++) {
+  //Writes Name and Height of all Pokemon to the DOM AND Labels biggest Pokemon  
+    if (pokemonList[i].height > 1.7) {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m) ' + big + '</p>');
+    } 
+    //Writes Name and Height of all Pokemon to the DOM
+    else {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)' + '</p>');
+    }
+}
+
