2d987f8731d14ada582aaecee92d7037fb13ea9f
Brett Ranieri <brett.ranieri@gmail.com>
2022-10-31 12:02:46 -0600

    create pokemonRepository IIFE and link to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
-let pokemonList = [
-    {name: 'Bulbasaur', type: ['Grass', 'poison'], height: .7},
-    {name: 'Ivysaur', type: ['Grass', 'poison'], height: 1},
-    {name: 'Venusaur', type: ['Grass', 'poison'], height: 2},
-    {name: 'Charmander', type: 'Fire', height: .6},
-    {name: 'Charmeleon', type: 'Fire', height: 1.1},
-    {name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7},
-    {name: 'Squirtle', type: 'Water', height: .5},
-    {name: 'Wartortle', type: 'Water', height: 1},
-    {name: 'Blastoise', type: 'Water', height: 1.6},
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: 'Bulbasaur', type: ['Grass', 'poison'], height: .7},
+        {name: 'Ivysaur', type: ['Grass', 'poison'], height: 1},
+        {name: 'Venusaur', type: ['Grass', 'poison'], height: 2},
+        {name: 'Charmander', type: 'Fire', height: .6},
+        {name: 'Charmeleon', type: 'Fire', height: 1.1},
+        {name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7},
+        {name: 'Squirtle', type: 'Water', height: .5},
+        {name: 'Wartortle', type: 'Water', height: 1},
+        {name: 'Blastoise', type: 'Water', height: 1.6},
+    ]; 
 
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    }
+})();
 
 let big = '- Wow, that\'s big!';
 
@@ -23,6 +37,6 @@ function loopList(pokemon){
         document.write('<div>' + pokemon.name + ' (height: ' + pokemon.height + ' m)' + '</div>');
     }
 }
-pokemonList.forEach(loopList);
+pokemonRepository.getAll().forEach(loopList);
 
 
