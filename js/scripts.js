bbf8ba1abcd3c1fe04fca1b76006dabea727458b
Brett Ranieri <brett.ranieri@gmail.com>
2022-11-06 11:02:41 -0700

    add loadList function

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,15 @@
 let pokemonRepository = (function() {
-    let dataSet = [
-        {name: 'Bulbasaur', type: ['Grass', 'poison'], height: .7},
-        {name: 'Ivysaur', type: ['Grass', 'poison'], height: 1},
-        {name: 'Venusaur', type: ['Grass', 'poison'], height: 2},
-        {name: 'Charmander', type: 'Fire', height: .6},
-        {name: 'Charmeleon', type: 'Fire', height: 1.1},
-        {name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7},
-        {name: 'Squirtle', type: 'Water', height: .5},
-        {name: 'Wartortle', type: 'Water', height: 1},
-        {name: 'Blastoise', type: 'Water', height: 1.6},
-    ]; 
+    let dataSet = []; 
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
         if (typeof pokemon === 'object') {
             //checks for inclusion of specific keys
             if (
-                Object.keys(pokemon).includes('name') &&
-                Object.keys(pokemon).includes('type') &&
-                Object.keys(pokemon).includes('height')
+                Object.keys(pokemon).includes('name') 
+                // ****Had to comment out to allow pokemon to meet requirements for push**** &&
+                //Object.keys(pokemon).includes('type') &&
+                //Object.keys(pokemon).includes('height')
             ) {
                 dataSet.push(pokemon);
             } else {
@@ -58,23 +50,39 @@ let pokemonRepository = (function() {
         return dataSet;
     }
 
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
+        loadList: loadList
     }
 })();
 //End of IIFE
 
-let big = '- Wow, that\'s big!';
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon){
+        pokemonRepository.addListItem(pokemon);
+    });
+});
 
-function loopList(pokemon){
-   pokemonRepository.addListItem(pokemon);
-}
 
-pokemonRepository.getAll().forEach(loopList);
 
-console.log(pokemonRepository.getAll());
 
 
 
